adb11a1d72cdd3aa6d23e10b0f02ef9073608845
Tamim Dari Chowdhury <tamim@kitegamesstudio.com>
2024-10-22 18:18:17 +0600

    [new-->] bcg data preprocessed.

--- Data_Preprocessing/Melting_columns_to_rows.py
+++ Data_Preprocessing/Melting_columns_to_rows.py
@@ -4,16 +4,16 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-dtp3_df = pd.read_excel("data/raw_vaccine_data/POL3_vaccine.xlsx")
+dtp3_df = pd.read_excel("data/raw_vaccine_data/BCG_vaccine.xlsx")
 
 dtp3_melted = dtp3_df.melt(id_vars=["unicef_region", "iso3", "country", "vaccine"], 
-                    var_name="Year", value_name="POL3")
+                    var_name="Year", value_name="BCG")
 
 # Convert the 'year' column to integer if necessary
 dtp3_melted['Year'] = dtp3_melted['Year'].astype(int)
 
 
-dtp3_melted.to_excel('data/vaccine_final/pol3_final.xlsx', index=None)
+dtp3_melted.to_excel('data/vaccine_final/bcg_final.xlsx', index=None)
 
 print('TOTAL DATA: ', len(dtp3_melted))
 print(dtp3_melted.head())
