10a835959acbcdb1c07651fb3dd17d9f1e363f32
Tamim Dari Chowdhury <tamim@kitegamesstudio.com>
2024-10-22 17:49:39 +0600

    [new-->] MCV1 vaccine data preprocessed.

--- Data_Preprocessing/Melting_columns_to_rows.py
+++ Data_Preprocessing/Melting_columns_to_rows.py
@@ -4,15 +4,15 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-dtp3_df = pd.read_excel("data/DTP3_vaccine_2000_2023.xlsx")
+dtp3_df = pd.read_excel("data/MCV1_vaccine.xlsx")
 
 dtp3_melted = dtp3_df.melt(id_vars=["unicef_region", "iso3", "country", "vaccine"], 
-                    var_name="year", value_name="DTP3")
+                    var_name="Year", value_name="DTP3")
 
 # Convert the 'year' column to integer if necessary
-dtp3_melted['year'] = dtp3_melted['year'].astype(int)
+dtp3_melted['Year'] = dtp3_melted['Year'].astype(int)
 
 
-dtp3_melted.to_excel('data/dtp3_final.xlsx', index=None)
+dtp3_melted.to_excel('data/vaccine_final/mcv1_final.xlsx', index=None)
 
 print(dtp3_melted.head())
