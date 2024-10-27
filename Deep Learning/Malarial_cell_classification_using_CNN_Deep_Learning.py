b4a56920c1697f6d03a8d8c8e4656381f7c5ccd9
Tamim Dari Chowdhury <dari.tamim028@gmail.com>
2024-10-28 02:26:39 +0600

    [new-->] Deep Learning folder added, malarial cell classification basic code added.

---
+++ Deep Learning/Malarial_cell_classification_using_CNN_Deep_Learning.py
@@ -0,0 +1,35 @@
+import keras
+import os
+
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+
+WIDTH = HEIGHT = 64
+
+INPUT_SHAPE = (WIDTH, HEIGHT, 3)
+inp = keras.layers.Input(shape=INPUT_SHAPE)
+
+conv1 = keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu', padding='same')(inp)
+pool1 = keras.layers.MaxPooling2D(pool_size=(2,2))(conv1)
+norm1 = keras.layers.BatchNormalization(axis=-1)(pool1)
+drop1 = keras.layers.Dropout(0.2)(norm1)
+
+conv2 = keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu', padding='same')(drop1)
+pool2 = keras.layers.MaxPooling2D(pool_size=(2,2))(conv2)
+norm2 = keras.layers.BatchNormalization(axis=-1)(pool2)
+drop2 = keras.layers.Dropout(0.2)(norm2)
+
+flat = keras.layers.Flatten()(drop2)
+
+hidden1 = keras.layers.Dense(512, activation='relu')(flat)
+norm3 = keras.layers.BatchNormalization(axis=-1)(hidden1)
+drop3 = keras.layers.Dropout(0.2)(norm3)
+
+hidden2 = keras.layers.Dense(256, activation='relu')(drop3)
+norm4 = keras.layers.BatchNormalization(axis=-1)(hidden2)
+drop4 = keras.layers.Dropout(0.2)(norm4)
+
+out = keras.layers.Dense(2, activation='sigmoid')(drop4)
+
+model = keras.Model(inputs=inp, outputs=out)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy')
+print(model.summary)
