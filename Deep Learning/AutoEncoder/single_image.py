4e708b67ef83b7d8ceefc041e1292c27236654d1
Tamim Dari Chowdhury <tamim@kitegamesstudio.com>
2024-12-26 18:48:58 +0600

    [update] code updated, executable.

--- Deep Learning/AutoEncoder/single_image.py
+++ Deep Learning/AutoEncoder/single_image.py
@@ -2,19 +2,15 @@
 from matplotlib.pyplot import imshow
 import numpy as np
 import cv2
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.models import Sequential
 
-from keras.preprocessing.image import img_to_array
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
-from keras.models import Sequencial
+np.random.seed(42)
 
 SIZE = 256
 
 img_data = []
-
-img = cv2.imread('images/monkey.jpg', 1)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.resize(img, (SIZE, SIZE))
-print('Event: Image is read, dimention', img.size)
+img = cv2.imread('images/monalis.jpg', 1)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(SIZE, SIZE, 3)))
