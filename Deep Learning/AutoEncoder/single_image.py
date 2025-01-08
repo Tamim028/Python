c5628d69f26d322adc59a732cfa763ba251b6b92
Tamim Dari Chowdhury <tamim@kitegamesstudio.com>
2025-01-08 19:18:06 +0600

    image data is normalized, 85b - An introduction to autoencoders - 15.44mins

--- Deep Learning/AutoEncoder/single_image.py
+++ Deep Learning/AutoEncoder/single_image.py
@@ -2,6 +2,8 @@
 from matplotlib.pyplot import imshow
 import numpy as np
 import cv2
+
+from keras.preprocessing.image import img_to_array
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from tensorflow.keras.models import Sequential
 
@@ -10,25 +12,34 @@ np.random.seed(42)
 SIZE = 256
 
 img_data = []
-img = cv2.imread('images/monalis.jpg', 1)
+img = cv2.imread('images/monkey.jpg', 1)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (SIZE, SIZE))
+img_data.append(img_to_array(img))
+print("Event: Successfully got image data")
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(SIZE, SIZE, 3)))
-model.add(MaxPooling2D((2, 2), padding='same'))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D((2, 2), padding='same'))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+img_array = np.reshape(img_data, (len(img_data), SIZE, SIZE, 3))
+#normalize image
+img_array = img_array.astype('float32')/255.0
+print("Event: Image data normalized using 255")
+
+# model = Sequential()
+# model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(SIZE, SIZE, 3)))
+# model.add(MaxPooling2D((2, 2), padding='same'))
+# model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+# model.add(MaxPooling2D((2, 2), padding='same'))
+# model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
  
 
-model.add(MaxPooling2D((2, 2), padding='same'))
+# model.add(MaxPooling2D((2, 2), padding='same'))
      
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(3, (3, 3), activation='relu', padding='same'))
+# model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+# model.add(UpSampling2D((2, 2)))
+# model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+# model.add(UpSampling2D((2, 2)))
+# model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+# model.add(UpSampling2D((2, 2)))
+# model.add(Conv2D(3, (3, 3), activation='relu', padding='same'))
 
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-model.summary()
+# model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+# model.summary()
